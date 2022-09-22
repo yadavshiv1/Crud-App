3ab3ca52e83fe66357b317411e71d21f0768ab21
Apple <apple@Apples-MacBook-Air-2.local>
2022-09-22 13:28:45 +0530

    first commit

---
+++ client/src/components/Navbaar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const Navbaar = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" to="/">CRUD APP</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbaar
